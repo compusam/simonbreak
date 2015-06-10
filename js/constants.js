26051d27de43b6a15758fd62e9c7ed1b7530bf69
José Antonio Chio <imagentlemail@gmail.com>
2015-06-10 02:10:05 -0500

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,11 +1,11 @@
 var constants = (function(){
 	return{
-		okSound: '../assets/good.mp3',
-		errorSound: '../assets/bad.mp3',
-		leftTop: '../assets/leftTop.mp3',
-		leftBottom: '../assets/leftBottom.mp3',
-		rightTop: '../assets/rightTop.mp3',
-		rightBottom: '../assets/rightBottom.mp3',
+		okSound: '../simonbreak/assets/good.mp3',
+		errorSound: '../simonbreak/assets/bad.mp3',
+		leftTop: '../simonbreak/assets/leftTop.mp3',
+		leftBottom: '../simonbreak/assets/leftBottom.mp3',
+		rightTop: '../simonbreak/assets/rightTop.mp3',
+		rightBottom: '../simonbreak/assets/rightBottom.mp3',
 		gameOverSound : '',
 		leftTopElementClass: '.simonLeftTop',
 		rightTopElementClass: '.simonRightTop',
