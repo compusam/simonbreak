b36dc29fb31512d3d2ccd0021a3bc92179149f89
José Antonio Chio <jose.chio@prodigious.com>
2015-06-10 02:14:20 -0500

    Removed unneeded cached files

--- build/.module-cache/210c4f84f26333388b90eff1f76e201e639c8263.js
+++
@@ -1,9 +0,0 @@
-var measuresBox = React.createClass({displayName: "measuresBox",
-  render: function(){
-    return(
-      React.createElement("div", {className: "measuresBox"}, 
-      "Hi, please, put your measures here"
-      )
-    );
-  }
-});
